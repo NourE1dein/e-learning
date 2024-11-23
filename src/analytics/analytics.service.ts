b1d229af033b80eb1856ec2030507a3ac4341c24
Abodonia911 <moabodonia7@gmail.com>
2024-11-23 16:35:29 +0200

    finalize schema

---
+++ src/analytics/analytics.service.ts
@@ -0,0 +1,6 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Progress } from './progress.schema';
+
+
