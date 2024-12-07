8e72a9cb83252d152f433b26e439f6bf95449b27
nour <noureldein.mostafa1@gmail.com>
2024-12-07 15:48:56 +0200

    Final backend and frontend

--- src/auth/auth.dto.ts
+++
@@ -1,7 +0,0 @@
-// Data Transfer Object for login and signup requests
-export class AuthDto {
-    email: string;
-    password: string;
-    role?: 'student' | 'instructor' | 'admin'; // Only needed for signup
-  }
-  
